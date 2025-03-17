d8d2c6cb1451625f6884653b281690a22626c7c0
dzamoranobenitt <90919155+dzamoranobenitt@users.noreply.github.com>
2025-03-17 16:35:33 -0300

    Cambios en el js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+// Funcioón para agregar amigos
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+    if (!nombre) {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    amigos.push(nombre);
+    input.value = "";
+}
+
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar la lista antes de agregar nuevos elementos
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+    let indice = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indice];
+    document.getElementById("resultado").innerHTML = `<li>${amigoSorteado}</li>`;
+}
